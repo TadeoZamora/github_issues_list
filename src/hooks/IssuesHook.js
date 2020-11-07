b28cc68c69e51f6ca08ef741cde8ebeab8097ad3
TadeoZamora <zamora.figueroa@gmail.com>
2020-11-06 19:15:58 -0700

    Add function to get pages and control pagination

--- src/hooks/IssuesHook.js
+++ src/hooks/IssuesHook.js
@@ -1,31 +1,62 @@
 import { useState } from "react";
 import http from "../services/https";
-
+//this custom hook its a helper to separate the logic and the api requests of the pure component
 const IssuesHook = () => {
+  //imported function to access to a custom service for http requests
   const { request } = http();
 
   const [issues, setIssues] = useState([]);
   const [issue, setIssue] = useState(null);
   const [comments, setComments] = useState([]);
+  const [pages, setPages] = useState(0);
 
-  const getIssues = async () => {
-    const { data } = await request.get("octocat/Hello-World/issues");
-    setIssues(data);
+  const getIssues = async (page = 1) => {
+    try {
+      const { data } = await request.get(
+        `octocat/Hello-World/issues?per_page=10&page=${page}`
+      );
+      setIssues(data);
+      getNumberIssues();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const getIssue = async (number) => {
-    const { data } = await request.get(`octocat/Hello-World/issues/${number}`);
-    setIssue(data);
-    getComment(data.number);
+    try {
+      const { data } = await request.get(
+        `octocat/Hello-World/issues/${number}`
+      );
+      setIssue(data);
+      getComment(data.number);
+    } catch (error) {
+      //TODO import alert component later
+      console.log(error);
+    }
+  };
+
+  const getNumberIssues = async () => {
+    try {
+      const { data } = await request.get(`octocat/Hello-World`);
+      setPages(data.open_issues_count);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const getComment = async (number) => {
-    const { data } = await request.get(
-      `octocat/Hello-World/issues/${number}/comments`
-    );
-    setComments(data);
+    try {
+      const { data } = await request.get(
+        `octocat/Hello-World/issues/${number}/comments`
+      );
+      setComments(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
-
+  const currentPage = async (page) => {
+    await getIssues(page);
+  };
   return {
     issues,
     issue,
@@ -33,7 +64,9 @@ const IssuesHook = () => {
     getIssues,
     getIssue,
     getComment,
-    comments
+    comments,
+    pages,
+    currentPage
   };
 };
 
