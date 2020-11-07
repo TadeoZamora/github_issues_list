cae2d92c05d2678429ae534834f3c76db0672ae3
TadeoZamora <zamora.figueroa@gmail.com>
2020-11-06 19:16:38 -0700

    add comments

--- src/services/https.js
+++ src/services/https.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-
+//it's created as a hook, because when we try to use tokens for security it's more simple to import or call auth functions
+//NOTE: im not going to use a DotEnv file to not expend more time
 const http = () => {
   const request = axios.create({
-    baseURL: "https://api.github.com/repos/",
+    baseURL: "https://api.github.com/repos/", //this value its better to set in a DotEnv file
     headers: {
       "Content-Type": "application/json"
     }
